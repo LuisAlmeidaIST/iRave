719892ee4898257467b7d3b11ee0cbc65f3c9c47
Tiago Lopes <tigas48@gmail.com>
2015-04-22 13:34:38 +0100

    commit

--- iRave.js
+++ iRave.js
@@ -1,6 +1,9 @@
 var estado=0;
 var prev =0;
 var voltar = 1;
+var announcement_palco = 0;
+var announcement_banda = 0;
+var announcement_estilo = 0;
 
 function getsTime(){
 	var today=new Date();
@@ -40,7 +43,10 @@ function colapseAll(){
 		document.getElementById("emergencia").style.display="none";
 		document.getElementById("emergencia-s").style.display="none";
 		document.getElementById("mute").style.display="none";
-		document.getElementById("noti").style.display="none";
+		document.getElementById("noti").style.display = "none";
+		document.getElementById("historico").style.display = "none";
+		document.getElementById("noti-add").style.display = "none";
+		document.getElementById("palco").style.display = "none";
 }
 function backs(){
 	if(voltar==0){
@@ -55,6 +61,38 @@ function backs(){
 function getEstado(){
 	return estado;
 }
+
+var scrolled = 0;
+
+$(document).ready(function () {
+
+
+    $("#downClick").on("click", function () {
+        scrolled = scrolled + 20;
+
+        $(".cover").animate({
+            scrollTop: scrolled
+        });
+
+    });
+
+
+    $("#upClick").on("click", function () {
+        scrolled = scrolled - 20;
+
+        $(".cover").animate({
+            scrollTop: scrolled
+        });
+
+    });
+
+
+    $(".clearValue").on("click", function () {
+        scrolled = 0;
+    });
+
+
+});
 
 var clicked = false, clickY;
 $(document).on({
@@ -81,47 +119,62 @@ function colapseCaixa(i){
 	prev = estado;
 	switch (i){
 		case 0:
-		document.getElementById("caixa1").style.display="inline";
-		prev= estado;
-		estado=0;
-		voltar=1;
+		    document.getElementById("caixa1").style.display="inline";
+		    prev= estado;
+		    estado=0;
+		    voltar=1;
 		break;
 		case 1:
-		document.getElementById("menu").style.display="inline";
-		prev= estado;
-		estado=1;
-		voltar=0;
+		    document.getElementById("menu").style.display="inline";
+		    prev= estado;
+		    estado=1;
+		    voltar=0;
 		break;
 		case 2:
-		document.getElementById("humor").style.display="inline";
-		prev= estado;
-		estado=2;
-		voltar=1;
+		    document.getElementById("humor").style.display="inline";
+		    prev= estado;
+		    estado=2;
+		    voltar=1;
 		break;
 		case 3:
-		document.getElementById("estado").style.display="inline";
-		prev= estado;
-		estado=3;
-		voltar=2;
+		    document.getElementById("estado").style.display="inline";
+		    prev= estado;
+		    estado=3;
+		    voltar=2;
 		break;
 		case 4:
-		document.getElementById("estatisticas").style.display="inline";
-		prev= estado;
-		estado=3;
-		voltar=2;
+		    document.getElementById("estatisticas").style.display="inline";
+		    prev= estado;
+		    estado=3;
+		    voltar=2;
 		break;
 		case 5:
-		document.getElementById("emergencia").style.display="inline";
-		prev= estado;
-		estado=3;
-		voltar=2;
+		    document.getElementById("emergencia").style.display="inline";
+		    prev= estado;
+		    estado=3;
+		    voltar=2;
 		break;
 		case 6:
-		document.getElementById("emergencia-s").style.display="inline";
-		prev= estado;
-		estado=0;
-		voltar=0;
+		    document.getElementById("emergencia-s").style.display="inline";
+		    prev= estado;
+		    estado=0;
+		    voltar=0;
 		break;
+	    case 7:
+	        colapseAll();
+	        document.getElementById("noti").style.display = "inline";
+	        getsTime();
+	        prev = estado;
+	        estado = 7;
+	        voltar = 1;
+	    break;
+        case 20:
+	        colapseAll();
+	        document.getElementById("noti-add").style.display = "inline";
+	        prev = estado;
+	        estado = 20;
+	        voltar = 7;
+	    break;
 	}
 }
 
@@ -129,27 +182,41 @@ function colapseCaixaBut1(i){
 	prev= estado;
 	switch (i){
 		case 0:
-		colapseAll();
-		document.getElementById("caixa1").style.display="inline";
-		prev= estado;
-		estado=0;
-		voltar=1;
-		break;
+		    colapseAll();
+		    document.getElementById("caixa1").style.display="inline";
+		    prev= estado;
+		    estado=0;
+		    voltar=1;
+		    break;
 		case 1:
-		colapseAll();
-		document.getElementById("noti").style.display="inline";
-		getsTime();
-		prev= estado;
-		estado=7;
-		voltar=1;
-		break;
-		case 2:
-		colapseAll();
-		document.getElementById("estado").style.display="inline";
-		prev= estado;
-		estado=3;
-		voltar=2;
-		break;
+		    colapseAll();
+		    document.getElementById("noti").style.display="inline";
+		    getsTime();
+		    prev= estado;
+		    estado=7;
+		    voltar=1;
+		    break;
+	    case 2:
+	        colapseAll();
+	        document.getElementById("estado").style.display = "inline";
+	        prev = estado;
+	        estado = 3;
+	        voltar = 2;
+	        break;
+	    case 7:
+	        colapseAll();
+	        document.getElementById("noti-add").style.display = "inline";
+	        prev = estado;
+	        estado = 20;
+	        voltar = 7;
+	        break;
+	    case 20:
+	        colapseAll();
+	        document.getElementById("palco").style.display = "inline";
+	        prev = estado;
+	        estado = 22;
+	        voltar = 20;
+	        break;
 	}
 }
 
@@ -157,37 +224,124 @@ function colapseCaixaBut2(i){
 	prev= estado;
 	switch (i){
 		case 0:
-		colapseAll();
-		document.getElementById("caixa1").style.display="inline";
-		prev= estado;
-		estado=0;
-		voltar=1;
-		break;
+		    colapseAll();
+		    document.getElementById("caixa1").style.display="inline";
+		    prev= estado;
+		    estado=0;
+		    voltar=1;
+		    break;
 		case 2:
-		colapseAll();
-		document.getElementById("estatisticas").style.display="inline";
-		prev= estado;
-		estado=4;
-		voltar=2;
-		break;
+		    colapseAll();
+		    document.getElementById("estatisticas").style.display="inline";
+		    prev= estado;
+		    estado=4;
+		    voltar=2;
+		    break;
 		case 1:
-		colapseAll();
-		document.getElementById("mute").style.display="inline";
-		getsTime();
-		prev= estado;
-		estado=6;
-		voltar=1;
-		break;
+		    colapseAll();
+		    document.getElementById("mute").style.display="inline";
+		    getsTime();
+		    prev= estado;
+		    estado=6;
+		    voltar=1;
+		    break;
 		case 5:
-		colapseAll();
-		document.getElementById("emergencia-s").style.display="inline";
-		getsTime();
-		prev= estado;
-		estado=0;
-		voltar=0;
-		break;
+		    colapseAll();
+		    document.getElementById("emergencia-s").style.display="inline";
+		    getsTime();
+		    prev= estado;
+		    estado=0;
+		    voltar=0;
+		    break;
+	    case 7:
+	        colapseAll();
+	        document.getElementById("noti-add").style.display = "inline";
+	        prev = estado;
+	        estado = 20;
+	        voltar = 7;
+	        break;
 	}
 }
+
+function notify_stage(f) {
+    switch(f){
+        case 1:
+            if(announcement_palco == 4 || announcement_palco == 1 || announcement_palco == 6 || announcement_palco == 9){
+                announcement_palco -= 1;
+                document.getElementById("b_p_1").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #007dc1), color-stop(1, #0061a7))";
+                document.getElementById("b_p_1").style.background = "-moz-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_1").style.background = "-webkit-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_1").style.background = "-o-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_1").style.background = "-ms-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_1").style.background = "linear-gradient(to bottom, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_1").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#007dc1', endColorstr='#0061a7',GradientType=0)";
+                document.getElementById("b_p_1").style.background = "#007dc1";
+            }
+            else {
+                announcement_palco += 1;
+                document.getElementById("b_p_1").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #d0451b), color-stop(1, #bc3315))";
+                document.getElementById("b_p_1").style.background = "-moz-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_1").style.background = "-webkit-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_1").style.background = "-o-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_1").style.background = "-ms-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_1").style.background = "linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_1").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#d0451b', endColorstr='#bc3315',GradientType=0)";
+                document.getElementById("b_p_1").style.background = "#d0451b";
+            }
+            break;
+        case 2:
+            if (announcement_palco == 4 || announcement_palco == 3 || announcement_palco == 8 || announcement_palco == 9) {
+                announcement_palco -= 3;
+                document.getElementById("b_p_2").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #007dc1), color-stop(1, #0061a7))";
+                document.getElementById("b_p_2").style.background = "-moz-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_2").style.background = "-webkit-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_2").style.background = "-o-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_2").style.background = "-ms-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_2").style.background = "linear-gradient(to bottom, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_2").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#007dc1', endColorstr='#0061a7',GradientType=0)";
+                document.getElementById("b_p_2").style.background = "#007dc1";
+            }
+            else {
+                announcement_palco += 3;
+                document.getElementById("b_p_2").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #d0451b), color-stop(1, #bc3315))";
+                document.getElementById("b_p_2").style.background = "-moz-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_2").style.background = "-webkit-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_2").style.background = "-o-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_2").style.background = "-ms-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_2").style.background = "linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_2").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#d0451b', endColorstr='#bc3315',GradientType=0)";
+                document.getElementById("b_p_2").style.background = "#d0451b";
+            }
+            break;
+        case 3:
+            if (announcement_palco == 6 || announcement_palco == 5 || announcement_palco == 8 || announcement_palco == 9) {
+                announcement_palco -= 5;
+                document.getElementById("b_p_3").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #007dc1), color-stop(1, #0061a7))";
+                document.getElementById("b_p_3").style.background = "-moz-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_3").style.background = "-webkit-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_3").style.background = "-o-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_3").style.background = "-ms-linear-gradient(top, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_3").style.background = "linear-gradient(to bottom, #007dc1 5%, #0061a7 100%)";
+                document.getElementById("b_p_3").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#007dc1', endColorstr='#0061a7',GradientType=0)";
+                document.getElementById("b_p_3").style.background = "#007dc1";
+            }
+            else {
+                announcement_palco += 5;
+                document.getElementById("b_p_3").style.background = "-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #d0451b), color-stop(1, #bc3315))";
+                document.getElementById("b_p_3").style.background = "-moz-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_3").style.background = "-webkit-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_3").style.background = "-o-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_3").style.background = "-ms-linear-gradient(top, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_3").style.background = "linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)";
+                document.getElementById("b_p_3").style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#d0451b', endColorstr='#bc3315',GradientType=0)";
+                document.getElementById("b_p_3").style.background = "#d0451b";
+            }
+            break;
+    }
+}
+
+
+
 
 function colapseCaixaBut3(i){
 	
@@ -216,6 +370,14 @@ function colapseCaixaBut3(i){
 		case 5:
 		colapseCaixa(prev);
 		break;
+	    case 7:
+	        colapseAll();
+	        document.getElementById("historico").style.display = "inline";
+	        prev = estado;
+	        estado = 21;
+	        voltar = 7;
+	        break;
+
 	
 	}
 }
